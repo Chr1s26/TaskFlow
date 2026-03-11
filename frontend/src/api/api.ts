93fd3a08a541bcc0f9530da0b9ccde88c06cdd48
Htet Kyaw Swar Linn <htetkyawswarlinn44@gmail.com>
2026-03-12 02:40:24 +0700

    updated

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,7 +1,14 @@
 import axios from "axios"
 
+// const api = axios.create({
+//   baseURL: "http://localhost:8084/api",
+//   headers: {
+//     "Content-Type": "application/json"
+//   }
+// })
+
 const api = axios.create({
-  baseURL: "http://localhost:8084/api",
+  baseURL: "/api",
   headers: {
     "Content-Type": "application/json"
   }
